f92a27e3b20a5f2b06edbdf3321f11c0f791c24b
mrhooverUCSC <61717155+mrhooverUCSC@users.noreply.github.com>
2021-04-30 00:43:17 -0700

    Question setup for four types of math

--- src/prefabs/Question.js
+++ src/prefabs/Question.js
@@ -1,19 +1,90 @@
 //When constructed, it takes in a difficulty, creates a question, and stores its information
-//For now, only addition of 2 numbers with 3 answers
-//Later, at least four tiers of difficulty, each of which includes the previous
-//  0: addition and subtraction
-//  1: multiplication
-//  2: division
-//  3: greatly increase range of questions
+//For now, only math of 2 numbers with 3 answers
+//four tiers of difficulty, each of which includes the previous
+//Creates a "e", or the full equation (mostly for debugging), and a "q", the equation without the answer (displayed in game)
+//Behind these is two constants, a true result, and two fakes.
 class Question{
-    constructor(){
-        this.addend1 = Phaser.Math.Between(1,9);
-        this.addend2 = Phaser.Math.Between(1,9);
-        this.result = this.addend1 + this.addend2;
-        this.e = this.addend1 + " + " + this.addend2 + " = " + this.result;
+    constructor(difficulty){
+        this.type = Phaser.Math.Between(0,difficulty);
+        console.log(this.type);
+        switch(this.type){
+            case 0:
+                this.addition();
+                break;
+            case 1:
+                this.subtraction();
+                break;
+            case 2:
+                this.multiplication();
+                break;
+            default:
+                this.division();
+        }
     }
 
-    equation(){
+    //Addition creates 2 numbers, finds the correct result, then varies it for the fakes
+    addition(){
+        this.constant1 = Phaser.Math.Between(-9,9);
+        this.constant2 = Phaser.Math.Between(-9,9);
+        this.result = this.constant1 + this.constant2;
+        do{
+            this.fake1 = this.result + Phaser.Math.Between(-5,5);
+        } while(this.fake1 == this.result)
+        do{
+            this.fake2 = this.result + Phaser.Math.Between(-5,5);
+        } while(this.fake2 == this.result || this.fake2 == this.fake1);
+        this.e = this.constant1 + " + " + this.constant2 + " = " + this.result;
+        this.q = this.constant1 + " + " + this.constant2 + " =";
+    }
+
+    //Subtraction, same as addition, but the second number cannot be negative for clarity
+    subtraction(){
+        this.constant1 = Phaser.Math.Between(-9,9);
+        this.constant2 = Phaser.Math.Between(1,9);
+        this.result = this.constant1 - this.constant2;
+        do{
+            this.fake1 = this.result + Phaser.Math.Between(-5,5);
+        } while(this.fake1 == this.result)
+        do{
+            this.fake2 = this.result + Phaser.Math.Between(-5,5);
+        } while(this.fake2 == this.result || this.fake2 == this.fake1);
+        this.e = this.constant1 + " - " + this.constant2 + " = " + this.result;
+        this.q = this.constant1 + " - " + this.constant2 + " =";
+    }
+
+    //Multiplication.  Fakes are based on the other numbers, not the result
+    multiplication(){
+        this.constant1 = Phaser.Math.Between(-9,9);
+        this.constant2 = Phaser.Math.Between(-9,9);
+        this.result = this.constant1 * this.constant2;
+        do{
+            this.fake1 = this.constant1 * Phaser.Math.Between(-9,9);
+        } while(this.fake1 == this.result)
+        do{
+            this.fake2 = this.constant2 * Phaser.Math.Between(-9,9);
+        } while(this.fake1 == this.result || this.fake2 == this.fake1)
+        this.e = this.constant1 + " * " + this.constant2 + " = " + this.result;
+        this.q = this.constant1 + " * " + this.constant2 + " =";
+    }
+
+    //Division.  Works backwards, making a multiplcation problem, then presenting it as division.
+    division(){
+        this.result = Phaser.Math.Between(-9,9);
+        this.constant1 = Phaser.Math.Between(1,9);
+        this.constant2 = this.A * this.B;
+        do{
+            this.fake1 = Phaser.Math.Between(-9,9);
+        } while(this.fake1 == this.result);
+        do{
+            this.fake2 = Phaser.Math.Between(-9,9);
+        } while(this.fake1 == this.result || this.fake2 == this.fake1);
+        this.e = this.C + " ÷ " + this.B + " = " + this.A;
+        this.q = this.C + " ÷ " + this.B + " =";
+    }
+
+    check_equation(){
         console.log(this.e);
+        console.log(this.fake1);
+        console.log(this.fake2);
     }
 }
