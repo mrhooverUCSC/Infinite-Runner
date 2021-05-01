b81c9e4cad6e8c4ccf6bd58cb6a97f96a9f02656
Ivan Martinez-Arias <50786762+UBSNASTy@users.noreply.github.com>
2021-04-30 21:48:33 -0700

    Movement + Speed in Play Scene

--- src/prefabs/Fish.js
+++ src/prefabs/Fish.js
@@ -9,13 +9,13 @@ class Fish extends Phaser.GameObjects.Sprite {
     }
 
     update() {
-        if(keyLEFT.isDown && this.x >= this.width) {
+        if(keyLEFT.isDown && this.x >= this.width - borderUISize - borderPadding) {
             this.x -= this.moveSpeed;
         }
         if(keyRIGHT.isDown && this.x <= game.config.width - this.width) {
             this.x += this.moveSpeed;
         }
-        if(keyUP.isDown && this.y >= this.height) {
+        if(keyUP.isDown && this.y >= this.height - borderUISize - borderPadding * 2) {
             this.y -= this.moveSpeed;
         }
         if(keyDOWN.isDown && this.y <= game.config.height - this.height) {
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,10 +8,20 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        // Ocean Background
         this.oceanBackground = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'oceanBackground').setOrigin(0, 0);
+        // Player
+        this.player1 = new Fish(this, game.config.width / 2, game.config.height / 2, 'fishPlayer').setOrigin(0.5, 0);
+        // Controls for the Scene
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
     }
 
     update() {
+        this.player1.update();
+
         this.oceanBackground.tilePositionX += oceanSpeed;
     }
 }
