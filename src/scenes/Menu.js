a93f6359049163cc4a74f491e20c738fc91a825c
mrhooverUCSC <61717155+mrhooverUCSC@users.noreply.github.com>
2021-05-01 03:00:48 -0700

    question setup scrolling

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,7 +5,6 @@ class Menu extends Phaser.Scene {
 
     preload() {
         this.load.image('fishPlayer', './assets/PlayersFish.png');
-        this.load.image('horizontal_bar', './assets/horizontal_bar.png');
     }
 
     create() {
@@ -26,19 +25,17 @@ class Menu extends Phaser.Scene {
         // show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Infinite Math', menuConfig).setOrigin(0.5);
         menuConfig.fontSize = '20px';
-        menuConfig.color = 
         this.add.text(game.config.width/2, game.config.height/2 - borderPadding, 'Controls: Arrow Keys', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize - borderPadding, 'Avoid the Obstacles and Swim Down the Correct Tunnel', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Press ENTER to go into Play Scene', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize * 2, 'Press ENTER to go into Play Scene', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
 
-        this.question = new Question(3);
-        this.question.check_equation();
+        //this.question = new Question(3);
+        //this.question.check_equation();
         //console.log(this.question.result);
 
         this.player1 = new Fish(this, game.config.width / 2, game.config.height / 2 + borderUISize, 'fishPlayer').setOrigin(0.5, 0);
-        new Obstacle(this, 2 * game.config.width/3, game.config.height / 2, 'horizontal_bar').setOrigin(0.5, 0);
 
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
@@ -46,8 +43,6 @@ class Menu extends Phaser.Scene {
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 
-//        this.question = new Question();
-//        this.question.equation();
     }
 
     update() {
